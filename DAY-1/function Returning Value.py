003c0d74252dbad866f744344c8505bf0acccd7d
A Harika <aharika@karunya.edu.in>
2026-06-02 20:50:37 +0530

    Add function to return hospital registration fee

---
+++ DAY-1/function Returning Value.py
@@ -0,0 +1,8 @@
+# Hospital Registration - Function Returning Value
+
+def get_registration_fee():
+    fee = 500
+    return fee
+
+amount = get_registration_fee()
+print("Registration Fee:", amount)
