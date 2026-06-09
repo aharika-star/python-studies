9ed20381d8d7eda32f109426e494d551a53a3bd0
A Harika <aharika@karunya.edu.in>
2026-06-09 20:50:31 +0530

    Delete Day-3 directory

--- Day-3/Exercise-5.py
+++
@@ -1,9 +0,0 @@
-patient = {
-    "patient_name": "Anjali",
-    "hospital": "City Care Hospital",
-    "disease": "Cold",
-    "doctor": "Dr. Sharma"
-}
-
-for key, value in patient.items():
-    print(key, ":", value)
