b4b2585da9483ccf14de9b6ff3077c77c93df477
A Harika <aharika@karunya.edu.in>
2026-06-09 20:34:03 +0530

    Delete DAY-1/function Returning Value.py

--- DAY-1/function Returning Value.py
+++
@@ -1,8 +0,0 @@
-# Hospital Registration - Function Returning Value
-
-def get_registration_fee():
-    fee = 500
-    return fee
-
-amount = get_registration_fee()
-print("Registration Fee:", amount)
